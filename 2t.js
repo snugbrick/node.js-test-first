bc98f133f76eb9e6e916eda145a2f93da879e492
圣迹 <76844630+snugbrick@users.noreply.github.com>
2024-01-08 21:24:12 +0800

    sb TypeScript

---
+++ 2t.js
@@ -0,0 +1,9 @@
+var myNum = [1, 2, 3];
+for (var _i = 0, myNum_1 = myNum; _i < myNum_1.length; _i++) {
+    var num = myNum_1[_i];
+    console.log(num);
+}
+var date = "sunDay";
+console.log(typeof date);
+date = 1;
+console.log(typeof date);
--- 2t.ts
+++ 2t.ts
@@ -2,3 +2,7 @@ var myNum = [1, 2, 3];
 for (let num of myNum) {
   console.log(num);
 }
+var date: number | string = "sunDay";
+console.log(typeof date);
+date = 1;
+console.log(typeof date);
