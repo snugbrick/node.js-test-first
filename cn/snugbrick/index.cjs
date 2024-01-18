327d8c454779e80b95c6f1cb0e94202c70163baf
圣迹 <76844630+snugbrick@users.noreply.github.com>
2024-01-18 18:31:31 +0800

    new

--- cn/snugbrick/index.cjs
+++ cn/snugbrick/index.cjs
@@ -1,3 +1,28 @@
 var mAdd = require('./2t.js')
+var fs = require('fs')
+var http = require('http')
+var events = require('events');
 
+var eventEmitter = new events.EventEmitter();
+var eventHandler = function () {
+    console.log("hi")
+}
+eventEmitter.on('eventName', eventHandler);
+eventEmitter.on('data_received', function () {
+    console.log('数据接收成功。');
+});
+
+eventEmitter.emit('eventName');
+eventEmitter.emit('data_received');
+//=========================================
+fs.readFile("t.txt", function (err, data) {
+    if (err) return console.error(err);
+    console.log(data.toString());
+})
 mAdd.add(1)
+
+http.createServer(function (req, res) {
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.write(data.toString());
+    res.end();
+});
