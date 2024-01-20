533ac1c15cefcaa59df133184bce57ed01f7ab4d
圣迹 <76844630+snugbrick@users.noreply.github.com>
2024-01-21 01:51:59 +0800

    nt git

--- cn/snugbrick/funcTest.cjs
+++ cn/snugbrick/funcTest.cjs
@@ -5,3 +5,32 @@ function excute(func, value) {
     func(value)
 }
 excute(say, "hi world")
+//=================================================
+var http = require("http");
+var url = require("url");
+
+function start(route) {
+    function onRequest(request, response) {
+        var pathname = url.parse(request.url).pathname;
+        console.log("Request for " + pathname + " received.");
+
+        router(pathname);
+
+        response.writeHead(200, { "Content-Type": "text/plain" });
+        response.write("sb git");
+        response.end();
+    }
+
+    http.createServer(onRequest).listen(8888);
+    console.log("Server has started.");
+}
+
+exports.start = start;
+
+function router(pathname) {
+    console.log("About to route a request for " + pathname);
+}
+
+exports.router = router;
+
+start(router.route);
